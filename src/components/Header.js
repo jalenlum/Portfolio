247efdf24b1d9c1839dc2985b0720ad0a612fea9
jalenlum <808jalen@gmail.com>
2023-10-09 10:31:20 -1000

    Started working on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 export const Header = () =>{
   return(
     <>
-
+      <div class="container">
+        <div class="row justify-content-center test">
+          <div class="col align-items-end">
+            <h2>Hello! I'm Jalen Lum</h2>
+            <p>
+              I'm a student studying Computer Science at the Univeristy of Hawai'i at Ma&#773;noa
+            </p>
+          </div>
+          <div class="col test">
+            hello
+          </div>
+        </div>
+      </div>
     </>
   )
 }
