4bcf2ca90fa068e82508b89a02440aa9c3cdd79c
jalenlum <808jalen@gmail.com>
2023-10-09 23:44:53 -1000

    added logo to browser and worked on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ export const Header = () =>{
   return(
     <>
       <div class="container">
-        <div class="row justify-content-center test">
-          <div class="col align-items-end">
-            <h2>Hello! I'm Jalen Lum</h2>
+        <div class="row justify-content-center home-info ">
+          <div class="col align-self-center introduction">
+            <h2 >Hello! I'm Jalen Lum.</h2>
             <p>
-              I'm a student studying Computer Science at the Univeristy of Hawai'i at Ma&#773;noa
+              I'm a college student studying Computer Science at the Univeristy of Hawai'i at Ma&#773;noa.
             </p>
           </div>
-          <div class="col test">
-            hello
+          <div class="col justify-content-center">
+            <img src="./images/headshot.jpg" class="headshot" alt="headshot photo"></img>
           </div>
         </div>
       </div>
