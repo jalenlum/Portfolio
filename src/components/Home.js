e7fd692ef7b014c0f41ea755c92d41f100a0e2a4
jalenlum <808jalen@gmail.com>
2023-10-24 23:59:44 -1000

    tried scalability again on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ export const Home = () =>{
     <>
       <div className="container" id="home">
         <div className="row justify-content-center" style={{marginTop:80}}>
-          <div className="col-lg align-self-center introduction">
+          <div className="col-12 col-lg-6 align-self-center introduction">
             <h2 style={{fontSize:60}}>Hello! I'm Jalen Lum.</h2>
             <p>
               I'm a college student studying Computer Science at the Univeristy of Hawai'i 
               at Ma&#773;noa.
             </p>
           </div>
-          <div className="col-lg d-flex justify-content-center">
+          <div className="col-12 col-lg-6 d-flex justify-content-center">
             <img 
               className="img-responsive"
               src="images/headshot.jpg" 
